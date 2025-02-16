d5c7db008f01abc78185de67307105608b493483
shahbaz-icover <shahbaz.alam@icoverinsure.us>
2025-02-16 13:11:15 +0530

    minor changes

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+// React.createElement ==> Object ==> HTML(DOM)
 const element = React.createElement("h1",{
     id: "title",
 }, "Hello world");
 
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+// JSX ==> React.createElement ==> Object ==> HTML(DOM)
+// Babel converts JSX to React.createElement
+
+const heading = (
+    <h1 id="headnig" className="">Namaste React</h1>
+)
+
+const HeaderComponent = () => {
+    return heading;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<HeaderComponent />);
